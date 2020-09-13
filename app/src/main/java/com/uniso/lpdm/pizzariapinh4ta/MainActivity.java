55f24bfc996f0e935b466ec9b25ddbe1ca419cd8
José Vitor <josevitorpignataro@gmail.com>
2020-09-13 14:28:49 -0300

    Comentários adicionados

--- app/src/main/java/com/uniso/lpdm/pizzariapinh4ta/MainActivity.java
+++ app/src/main/java/com/uniso/lpdm/pizzariapinh4ta/MainActivity.java
@@ -17,16 +17,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    //Método que chama o textview referente a horas quando clicar
     public void onclickhourCheck(View view){
         TextView chkHours = findViewById(R.id.RegHours);
         chkHours.setText("Total de horas!");
     }
+    //Método que chama o textview referente a pedindo quando executar o click
     public void OnClickOrder(View view){
         TextView orderfood = findViewById(R.id.RegiPed);
         orderfood.setText("Registrando pedido...");
     }
 
+    //Método que chama a segunda atividade que nos leva a cadastro de pedido para a cozinha
     public void OnClickKitchen(View view){
     Intent intent = new Intent(this, KItchenActivity.class);
     startActivity(intent);
